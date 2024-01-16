94ffb3cf83192d00522dcd40a5bdcf34fd59992a
Ade Iriani Sapitri <adesapitri13@gmail.com>
2024-01-16 19:03:55 +0700

    Pengumpulan Tugas 2A Type Data

---
+++ Tugas-2A-TypeData/materi.dart
@@ -0,0 +1,23 @@
+// void main (){
+//   print("Helloi");
+// }
+
+// DATA INPUT OUTPUT
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   print("masukan password :");
+//   String? inputText = stdin.readLineSync();
+//   print("Password:"+ inputText!);
+// }
+
+// VARIABEL
+void main(){
+  var name = "ade";
+  var angka = 13;
+  var todayIsFriday = false;
+
+  print(name);
+  print(angka);
+  print(todayIsFriday);
+}
---
+++ Tugas-2A-TypeData/type.dart
@@ -0,0 +1,78 @@
+// Soal No 1 (Membuat Kalimat)
+
+// void main(){
+//   var word = 'dart';
+//   var second = 'is';
+//   var third = 'awesome';
+//   var fourth = 'and';
+//   var fifth = 'I';
+//   var sixth = 'love';
+//   var seventh = 'it!';
+
+//   var kalimat = '$word $second $third $fourth $fifth $sixth $seventh';
+//   print(kalimat);
+// }
+
+// Soal No.2 Mengurai kalimat (Akses karakter dalam string),
+
+// void main(){
+//   var sentence = "I am going to be Flutter Developer";
+//   var FirstWord = sentence[0];
+//   var SecondWord = sentence[2] + sentence[3];
+//   var thirdWord = sentence.substring(5, 10);
+//   var fourthWord = sentence.substring(11, 13);
+//   var fifthWord = sentence.substring(14, 16);
+//   var sixthWord = sentence.substring(17, 24);
+//   var seventhWord = sentence.substring(24);
+
+//   print('First Word: ' + FirstWord);
+//   print('Second Word: ' + SecondWord);
+//   print('Third Word: ' + thirdWord);
+//   print('Fourth Word: ' + fourthWord);
+//   print('Fifth Word: ' + fifthWord);
+//   print('Sixth Word: ' + sixthWord);
+//   print('Seventh Word: ' + seventhWord);
+
+// }
+
+// SOAL NO 3 I/O
+// import 'dart:io';
+
+// void main() {
+//   print('Masukkan nama depan: ');
+//   String firstName = stdin.readLineSync()!;
+
+//   print('Masukkan nama belakang: ');
+//   String lastName = stdin.readLineSync()!;
+
+//   String fullName = '$firstName $lastName';
+
+//   print('Nama lengkap: $fullName');
+// }
+
+// soal no 4
+void main() {
+  int a = 5;
+  int b = 10;
+
+  // Operasi perkalian
+  int multiplicationResult = a * b;
+  print('perkalian = $multiplicationResult');
+
+  // Operasi pembagian
+  double divisionResult = a / b;
+  print('pembagian = $divisionResult');
+
+  // Operasi penjumlahan
+  int additionResult = a + b;
+  print('penjumlahan = $additionResult');
+
+  // Operasi pengurangan
+  int subtractionResult = a - b;
+  print('pengurangan = $subtractionResult');
+
+}
+
+
+
+
